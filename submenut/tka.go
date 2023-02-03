e853439ad8c39f76d4d184477ccaf276019e886a
tonivaa <tonivaa@pm.me>
2023-02-03 21:57:26 +0200

    testi

--- submenut/tka.go
+++ submenut/tka.go
@@ -1,7 +1,46 @@
 package submenut
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/eiannone/keyboard"
+	"github.com/tonivaa/hellafly/hinnat"
+)
 
 func Toka() {
 	fmt.Println("toka submenu")
+
+	err := keyboard.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		keyboard.Close()
+	}()
+
+	for {
+		fmt.Println("Kerro jotai")
+
+		char, key, err := keyboard.GetSingleKey()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if key == keyboard.KeyBackspace2 {
+			fmt.Println("Palataan..")
+			break
+		}
+
+		i, _ := strconv.Atoi(string(char))
+
+		_, ok := hinnat.Maalaus[i]
+		if ok {
+			fmt.Println("Tähän tulee", i, hinnat.Maalaus[i])
+		} else {
+			fmt.Println("Ei löydy:", i)
+		}
+
+	}
 }
